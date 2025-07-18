c0f39d1ccc374ea87c8e99286584a285c8012fd2
Damn1self <damn1self@gmail.com>
2025-07-18 01:19:56 -0500

    Manejando excepciones del API

---
+++ Producto/INCHE.Producto.Application/Exceptions/ExceptionManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using INCHE.Producto.Application.Features;
+
+//using INCHE.Producto.Application.External.ApplicationInsights;
+
+//using INCHE.Producto.Common.Constants;
+//using INCHE.Producto.Domain.Models.ApplicationInsights;
+
+namespace INCHE.Producto.Application.Exceptions
+{
+    public class ExceptionManager : IExceptionFilter
+    {
+        //private readonly IInsertApplicationInsightsService _insertApplicationInsightsService;
+        //public ExceptionManager(IInsertApplicationInsightsService insertApplicationInsightsService)
+        //{
+        //    _insertApplicationInsightsService = insertApplicationInsightsService;
+        //}
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ObjectResult(ResponseApiService.Response(
+                StatusCodes.Status500InternalServerError, null, context.Exception.Message));
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            //var metric = new InsertApplicationInsightsModel(
+            //    ApplicationInsightsConstants.METRIC_TYPE_ERROR,
+            //    context.Exception.Message,
+            //    context.Exception.ToString());
+
+            //_insertApplicationInsightsService.Execute(metric);
+        }
+    }
+}
---
+++ Producto/INCHE.Producto.Common/Constants/ApplicationConstants.cs
@@ -0,0 +1,28 @@
+﻿
+namespace INCHE.Producto.Common.Constants
+{
+    public class ApplicationConstants
+    {
+		public static readonly decimal MARGEN_PRECIO_VENTA = 1.13M;
+	}
+
+	public static class Messages
+	{
+
+		public const string RecordCreated = "El registro se creó correctamente";
+		public const string RecordRetrieved = "El registro se obtuvo correctamente";
+		public const string RecordsRetrieved = "Los registros se obtuvieron correctamente";
+		public const string RecordUpdated = "El registro se actualizó correctamente";
+		public const string RecordDeleted = "El registro se eliminó correctamente";
+		public const string RecordNotFound = "No se encontró el registro";
+		public const string NoRecordsFound = "No se encontraron registros";
+		public const string RecordAlreadyDeleted = "El registro ya ha sido eliminado";
+		public const string RecordAlreadyExists = "El registro ya existe";
+		public const string RecordCreationFailed = "No se pudo crear el registro";
+		public const string RecordUpdateFailed = "No se pudo actualizar el registro";
+		public const string RecordDeleteFailed = "No se pudo eliminar el registro";
+		public const string InvalidRecordId = "El identificador no es válido";
+		public const string MissingData = "Faltan datos obligatorios";
+
+	}
+}
