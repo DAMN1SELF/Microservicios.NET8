91a10e2d10b396d913644122a32a0733f79183ce
Damn1self <damn1self@gmail.com>
2025-07-18 06:43:38 -0500

    CQRS de Usuario

---
+++ Producto/INCHE.Producto.Application/DataBase/User/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using INCHE.Producto.Common.Constants;
+using INCHE.Producto.Domain.Entities.User;
+
+namespace INCHE.Producto.Application.DataBase.User.Commands.CreateUser
+{
+    public class CreateUserCommand: ICreateUserCommand
+    {
+        private readonly IDataBaseService _databaseService;
+        private readonly IMapper _mapper;
+        public CreateUserCommand(IDataBaseService databaseService, IMapper mapper)
+        {
+            _databaseService = databaseService;
+            _mapper = mapper;
+        }
+        public async Task<CreateUserModel> Execute(CreateUserModel model)
+        {
+            try
+            {
+				var entity = _mapper.Map<UserEntity>(model);
+				await _databaseService.User.AddAsync(entity);
+				await _databaseService.SaveAsync();
+				return model;
+			}
+            catch (Exception ex)
+            {
+
+				throw new ApplicationException(Messages.RecordCreationFailed, ex);
+			}
+        }
+    }
+}
