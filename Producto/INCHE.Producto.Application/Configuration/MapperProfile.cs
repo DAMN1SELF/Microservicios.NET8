4662f06ed02922e2f8e5101aaf3c787705f452f0
Damn1self <damn1self@gmail.com>
2025-07-17 21:36:18 -0500

    MapperProfile de CQRS

--- Producto/INCHE.Producto.Application/Configuration/MapperProfile.cs
+++ Producto/INCHE.Producto.Application/Configuration/MapperProfile.cs
@@ -1,9 +1,9 @@
 ﻿using AutoMapper;
-//using INCHE.Producto.Application.DataBase.Producto.Commands.CreateProducto;
-//using INCHE.Producto.Application.DataBase.Producto.Commands.UpdateProducto;
-//using INCHE.Producto.Application.DataBase.Producto.Queries.GetAllProductos;
-//using INCHE.Producto.Application.DataBase.Producto.Queries.GetProductoByDocumentNumber;
-//using INCHE.Producto.Application.DataBase.Producto.Queries.GetProductoById;
+using INCHE.Producto.Application.DataBase.Product.Commands.CreateProduct;
+using INCHE.Producto.Application.DataBase.Product.Commands.UpdateProduct;
+using INCHE.Producto.Application.DataBase.Product.Queries.GetAllProducts;
+using INCHE.Producto.Application.DataBase.Product.Queries.GetProductById;
+using INCHE.Producto.Application.DataBase.Product.Queries.GetProductByName;
 using INCHE.Producto.Domain.Entities;
 
 namespace INCHE.Producto.Application.Configuration
@@ -14,11 +14,11 @@ namespace INCHE.Producto.Application.Configuration
         {
 
             #region Producto
-            //CreateMap<ProductoEntity, CreateProductoModel>().ReverseMap();
-            //CreateMap<ProductoEntity, UpdateProductoModel>().ReverseMap();
-            //CreateMap<ProductoEntity, GetAllProductoModel>().ReverseMap();
-            //CreateMap<ProductoEntity, GetProductoByIdModel>().ReverseMap();
-            //CreateMap<ProductoEntity, GetProductoByDocumentNumberModel>().ReverseMap();
+            CreateMap<ProductoEntity, CreateProductModel>().ReverseMap();
+            CreateMap<ProductoEntity, UpdateProductModel>().ReverseMap();
+            CreateMap<ProductoEntity, GetAllProductModel>().ReverseMap();
+            CreateMap<ProductoEntity, GetProductByIdModel>().ReverseMap();
+            CreateMap<ProductoEntity, GetProductByNameModel>().ReverseMap();
 			#endregion
 		}
 	}
