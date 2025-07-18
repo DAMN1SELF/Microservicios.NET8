f363ba693a10ba01eb8364c3a04f4d3d6a08f089
Damn1self <damn1self@gmail.com>
2025-07-18 07:56:40 -0500

    Modulo Persistencia -> Configuracion de Database

--- Producto/INCHE.Producto.Persistence/DataBase/DataBaseService.cs
+++ Producto/INCHE.Producto.Persistence/DataBase/DataBaseService.cs
@@ -1,7 +1,15 @@
-﻿using INCHE.Producto.Persistence.Configuration;
-using INCHE.Producto.Application.DataBase;
+﻿using INCHE.Producto.Application.DataBase;
+using INCHE.Producto.Domain.Entities.Compras;
+using INCHE.Producto.Domain.Entities.Movimientos;
 using INCHE.Producto.Domain.Entities.Producto;
 using INCHE.Producto.Domain.Entities.User;
+using INCHE.Producto.Domain.Entities.Ventas;
+using INCHE.Producto.Persistence.Configuration.Producto;
+using INCHE.Producto.Persistence.Configuration.Usuario;
+using INCHE.Producto.Persistence.Configuration.Compras;
+using INCHE.Producto.Persistence.Configuration.Ventas;
+using INCHE.Producto.Persistence.Configuration.Movimientos;
+
 using Microsoft.EntityFrameworkCore;
 
 namespace INCHE.Producto.Persistence.DataBase
@@ -16,7 +24,26 @@ namespace INCHE.Producto.Persistence.DataBase
 		public DbSet<UserEntity> User { get; set; }
 		public DbSet<ProductoEntity> Producto { get; set; }
 
-        public async Task<bool> SaveAsync()
+
+		#region Compras
+		public DbSet<CompraCab> CompraCab { get; set; }
+		public DbSet<CompraDet> CompraDet { get; set; }
+		#endregion
+
+
+		#region Compras
+		public DbSet<VentaCab> VentaCab { get; set; }
+		public DbSet<VentaDet> VentaDet { get; set; }
+		#endregion
+
+
+		#region Movimientos
+		public DbSet<MovimientoCab> MovimientoCab { get; set; }
+		public DbSet<MovimientoDet> MovimientoDet { get; set; }
+		#endregion
+
+
+		public async Task<bool> SaveAsync()
         {
             return await SaveChangesAsync() > 0;
         }
@@ -30,7 +57,13 @@ namespace INCHE.Producto.Persistence.DataBase
 		{
 			new UserConfiguration(modelBuilder.Entity<UserEntity>());
 			new ProductoConfiguration(modelBuilder.Entity<ProductoEntity>());
-        }
+			new CompraCabConfiguration(modelBuilder.Entity<CompraCab>());
+			new CompraDetConfiguration(modelBuilder.Entity<CompraDet>());
+			new VentaCabConfiguration(modelBuilder.Entity<VentaCab>());
+			new VentaDetConfiguration(modelBuilder.Entity<VentaDet>());
+			new MovimientoCabConfiguration(modelBuilder.Entity<MovimientoCab>());
+			new MovimientoDetConfiguration(modelBuilder.Entity<MovimientoDet>());
+		}
 
     }
 }
